6e92e9ff2c8d2e2a231429887682a9101ec554f5
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2025-04-06 19:40:19 +0800

    :technologist: (Telescope) Remove filtering

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -18,16 +18,8 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
 
         $this->hideSensitiveRequestDetails();
 
-        $isLocal = $this->app->environment('local');
-
-        Telescope::filter(function (IncomingEntry $entry) use ($isLocal) {
-            return $isLocal ||
-                   $entry->isReportableException() ||
-                   $entry->isFailedRequest() ||
-                   $entry->isFailedJob() ||
-                   $entry->isScheduledTask() ||
-                   $entry->hasMonitoredTag();
-        });
+        // To filter data on production, use filtering. Currently allow all data.
+        // https://laravel.com/docs/12.x/telescope#filtering-entries
     }
 
     /**
