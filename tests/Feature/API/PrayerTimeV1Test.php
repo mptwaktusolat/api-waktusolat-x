06da5a6fc5cd11b7faa8b7428f0ed404bc437d48
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2025-04-10 20:43:06 +0800

    :test_tube: Update Test cases

--- tests/Feature/API/PrayerTimeV1Test.php
+++ tests/Feature/API/PrayerTimeV1Test.php
@@ -1,13 +1,11 @@
 <?php
 
-use Illuminate\Testing\Fluent\AssertableJson;
-
 test('get prayer time by month (no param)', function () {
     $response = $this->getJson('/api/solat/sgr01');
 
     $response->assertStatus(200);
 
-    $response->assertJsonStructure([
+    $response->assertExactJsonStructure([
         'prayerTime' => [
             '*' => [
                 'hijri',
@@ -19,27 +17,20 @@ test('get prayer time by month (no param)', function () {
                 'asr',
                 'maghrib',
                 'isha',
-            ]
-        ]
+            ],
+        ],
+        'status',
+        'serverTime',
+        'periodType',
+        'lang',
+        'zone',
+        'bearing',
     ]);
 
-    // TODO: Check if the month and year of the prayer times match the current month and year
-
-    // $currentMonth = now()->format('m');
-    // $currentYear = now()->format('Y');
-
-    // foreach ($prayerTimes as $prayerTime) {
-    //     $date = \Carbon\Carbon::createFromFormat('d-M-Y', $prayerTime['date']);
-    //     $this->assertEquals($currentMonth, $date->format('m'), 'Month should match the current month.');
-    //     $this->assertEquals($currentYear, $date->format('Y'), 'Year should match the current year.');
-    // }
-
-    // $response->assertJson(function (AssertableJson $json) {
-    //     $json->has('prayerTime')
-    //         ->where('status', 'OK!')
-    //         ->where('periodType', 'month')
-    //         ->where('zone', 'SGR01')
-    //         ->whereType('prayerTime', 'array')
-    //         ->etc();
-    // });
+    $response
+        ->assertJsonPath('status', 'OK!')
+        ->assertJsonPath('periodType', 'month')
+        ->assertJsonPath('lang', '')
+        ->assertJsonPath('bearing', '')
+        ->assertJsonPath('zone', 'SGR01');
 });
