34d20b45857640fea3d9b6d63498730252e5c7a8
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2025-08-05 20:58:40 +0800

    ✨ add blade icons and ionicons packages; update home view to use ionicon component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -58,7 +58,7 @@
                         <a href="/docs"
                             class="inline-flex h-9 items-center justify-center rounded-md bg-gray-900 px-4 py-2 sm:px-6 sm:py-4 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/80 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-pink-500 dark:text-white dark:hover:bg-pink-600 dark:focus-visible:ring-pink-300 group">
                             <span>Get Started</span>
-                            <ChevronRight class="h-4 w-4 translate-x-1 group-hover:translate-x-2 transition" />
+                            <x-ionicon-chevron-forward class="h-4 w-4 ml-1" />
                         </a>
                         <a href="/health"
                             class="inline-flex h-9 items-center justify-center rounded-md border border-gray-200 bg-white px-4 py-2 sm:px-6 sm:py-4 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-pink-500 dark:bg-transparent dark:text-pink-300 dark:hover:bg-pink-500/10 dark:hover:text-pink-200 dark:focus-visible:ring-pink-300">
