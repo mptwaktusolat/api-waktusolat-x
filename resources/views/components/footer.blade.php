2ccda2265abb2f0b915e9c8ef8c30a987bce8b03
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2025-08-05 20:53:03 +0800

    ✨ add footer component

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,15 @@
+<footer
+    class="flex flex-col sm:flex-row w-full items-center md:h-12 h-18 px-4 border-t md:px-6 dark:border-gray-800 bg-white dark:bg-gray-950 py-4 sm:py-0">
+    <p class="text-sm text-gray-500 dark:text-gray-400">
+        © 2025
+        <a class="hover:underline underline-offset-4 dark:text-pink-300 dark:hover:text-pink-200"
+            href="https://waktusolat.app" target="_blank" rel="noopener noreferrer">
+            Waktu Solat Project
+        </a>
+    </p>
+    <nav class="mt-2 sm:mt-0 sm:ml-auto flex flex-wrap gap-4 sm:gap-6">
+        <a class="text-sm hover:underline underline-offset-4 dark:text-gray-300 dark:hover:text-pink-200" href="/about">
+            About
+        </a>
+    </nav>
+</footer>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -69,6 +69,7 @@
             </div>
         </section>
     </main>
+    <x-footer />
 </body>
 
 </html>
