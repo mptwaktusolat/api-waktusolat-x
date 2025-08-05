8f87b5aab9977cd0050a3b91d6535aeced8f84e4
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2025-08-06 02:19:21 +0800

    🐛 remove classname

--- resources/views/components/top-banner.blade.php
+++ resources/views/components/top-banner.blade.php
@@ -7,8 +7,6 @@
         </div>
     </div>
     <div aria-hidden="true"
-        className="absolute left-[max(45rem,calc(50%+8rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
-        aria-hidden="true"
         class="absolute left-[max(45rem,calc(50%+8rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl">
         <div style="clip-path: polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%);"
             class="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30 dark:from-[#80b5ff] dark:to-[#9089fc] dark:opacity-20">
