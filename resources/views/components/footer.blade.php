9871f1a2067cebf9e5cea0ae9eeb402045a7359d
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2026-02-14 02:40:27 +0800

    :pencil2: update footer copyright year to 2026. Closes #38

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <footer
     class="flex flex-col sm:flex-row w-full items-center md:h-12 h-18 px-4 border-t border-gray-200 md:px-6 dark:border-gray-800 bg-white dark:bg-gray-950 py-4 sm:py-0">
     <p class="text-sm text-gray-500 dark:text-gray-400">
-        © 2025
+        © 2026
         <a class="hover:underline underline-offset-4 dark:text-pink-300 dark:hover:text-pink-200"
             href="https://waktusolat.app" target="_blank" rel="noopener noreferrer">
             Waktu Solat Project
