f4b86dfae16e85e383bb52b09a3f9c64b98354a3
fareez <iqmal3@outlook.com>
2026-03-20 23:57:42 +0800

    👔 add debug-headers route to debug forwarded headers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DataHealthController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,4 +18,14 @@ Route::get('/locations', function () {
 
 Route::get('/health', [DataHealthController::class, 'index'])->name('data-health');
 
-require __DIR__ . '/auth.php';
+Route::get('/debug-headers', function (Request $request) {
+    return [
+        'is_secure' => $request->isSecure(),
+        'scheme' => $request->getScheme(),
+        'forwarded_proto' => $request->header('X-Forwarded-Proto'),
+        'forwarded_for' => $request->header('X-Forwarded-For'),
+        'url' => $request->url(),
+    ];
+});
+
+require __DIR__.'/auth.php';
