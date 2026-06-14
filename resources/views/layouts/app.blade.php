6bcfca033f4edae5ed80c8c45c15faa486ee80c6
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2026-06-14 17:15:07 +0800

    :bento: add sizes any to svg favicons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
 
     <!-- favicons -->
     <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
-    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="icon" type="image/svg+xml" sizes="any" href="/favicon.svg" />
     <link rel="shortcut icon" href="/favicon.ico" />
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
     <meta name="apple-mobile-web-app-title" content="Waktu Solat API" />
